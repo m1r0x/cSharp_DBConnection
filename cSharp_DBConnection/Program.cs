300743c989c3d2d8ca2eab44f507275a9bf0e6b1
Strati <m1r0x@live.de>
2018-04-21 11:30:25 +0200

    initial commit

--- cSharp_DBConnection/Program.cs
+++ cSharp_DBConnection/Program.cs
@@ -10,6 +10,51 @@ namespace cSharp_DBConnection
     {
         static void Main(string[] args)
         {
+            using (Entities ctx = new Entities())
+            {
+                foreach(Category c in ctx.Categories)
+                {
+                    Console.WriteLine("::::::::" + c.CategoryName + "::::::::");
+                    foreach(Product p in c.Products)
+                    {
+                        Console.WriteLine("+" + p.ProductName);
+                    }
+                }
+
+                Console.WriteLine("----------------------------------------------");
+                Console.WriteLine("Press Key to add new Product in Category: Bike");
+                Console.ReadKey();
+
+                //Neues Produkt erstellen
+                Product p1 = new Product();
+                p1.CategoryID = 3;
+                p1.ProductName = "MX800 Superbike";
+
+
+                //Neues Produkt in Datenbank speichern
+                ctx.Products.Add(p1);
+                Console.WriteLine("New Product added");
+                //Änderungen speichern
+                ctx.SaveChanges();
+                Console.WriteLine("Changes saved");
+
+                Console.WriteLine("----------------------------------------------");
+                Console.WriteLine("Press Key to reload Products");
+                Console.ReadKey();
+
+                foreach (Category c in ctx.Categories)
+                {
+                    Console.WriteLine("::::::::" + c.CategoryName + "::::::::");
+                    foreach (Product p in c.Products)
+                    {
+                        Console.WriteLine("+" + p.ProductName);
+                    }
+                }
+
+                Console.WriteLine("----------------------------------------------");
+                Console.WriteLine("Press Key to Exit");
+                Console.ReadKey();
+            }
         }
     }
 }
